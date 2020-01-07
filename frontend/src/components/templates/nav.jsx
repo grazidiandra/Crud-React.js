5dca63650887826cf769aa4e7ca8c2709e0f28ec
grazidiandra <grazi.diandra@gmail.com>
2020-01-07 17:19:38 -0300

    component nav

--- frontend/src/components/templates/nav.jsx
+++ frontend/src/components/templates/nav.jsx
@@ -3,6 +3,13 @@ import './nav.css';
 
 export default props => 
   <aside className="menu-area">
-    Navegação
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usuários
+      </a>
+    </nav>
   </aside>
   
