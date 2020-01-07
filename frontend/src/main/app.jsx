fd4b464ce6652c19e9b2ae122e6af2cbf249bfbc
grazidiandra <grazi.diandra@gmail.com>
2020-01-07 16:50:08 -0300

    component header

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './app.css';
 
 import Logo from '../components/templates/logo';
@@ -10,6 +12,6 @@ export default props =>
   <div className="app">
     <Logo />
     <Nav />
-    <Main />
+    <Main icon="home" title="Início" subtitle="Projeto React.js"/>
     <Footer />
-  </div>
+  </div> 
